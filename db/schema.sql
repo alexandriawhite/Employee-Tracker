819f78c98d07ed5188979c09325332c9dec2b0b1
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-09 21:31:12 -0600

    Updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,21 +11,25 @@ CREATE TABLE department(
 
 CREATE TABLE role(
     id INT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     PRIMARY KEY(id)
     FOREIGN KEY(department_id) 
     REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
     id INT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
     FOREIGN KEY(role_id)
-    REFERENCES role(id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 )
