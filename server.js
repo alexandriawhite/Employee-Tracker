3c25ec21e5249101c9e11532522ffd312139b511
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2023-01-12 17:37:09 -0600

    Employee table is getting closer to having the manager pull properly

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ inquirer
     .then((answers) => {
         // console.log(answers.options)
         // department();
-        // employee();
-        role();
+        employee();
+        // role();
     })
     .catch((error) => {
         if (error.isTtyError) {
@@ -92,13 +92,13 @@ const role = () => {
 //Need to correct manager - right now it is pulling as the person itself
 const employee = () => {
     db.query(`
-    SELECT e.id,
+    SELECT e.id AS employee_id,
     e.first_name,
     e.last_name,
-    r.title,
+    r.title AS job_title,
     d.name AS department,
     r.salary, 
-    concat(e.first_name, ' ', e.last_name) AS manager
+    IFNULL(e.manager_id, 'e.first_name, ' ', e.last_name') AS manager
     FROM employee e
     LEFT JOIN role r ON e.role_id = r.id
     LEFT JOIN department d ON d.id = r.department_id`,
@@ -109,3 +109,6 @@ const employee = () => {
             console.table(res);
         });
 }
+
+
+
